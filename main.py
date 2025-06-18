7a69a5d6759b157ef34aab59e8b8a414f609ca32
SuperYoshi10000 <148275327+SuperYoshi10000@users.noreply.github.com>
2025-06-18 16:30:50 +0000

    switch to soapysdr

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import os
 from dotenv import load_dotenv
-from rtlsdr import RtlSdrTcpClient
+
 import SoapySDR
+from SoapySDR import Device
+from SoapySDR import *
+import numpy as np
 
 load_dotenv()
 
-sdr = RtlSdrTcpClient(
-    hostname=os.getenv("RTLSDR_HOSTNAME", "localhost"),
-    port=int(os.getenv("RTLSDR_PORT", 8000)),
-    test_mode_enabled=bool(os.getenv("RTLSDR_TEST_MODE", False))
-)
+server=os.getenv("RTLSDR_SERVER", "localhost:8000")
+print(server)
+sdr = Device(dict(
+    driver="rtltcp",
+    rtltcp=server,
+))
 
 sample_rate = os.getenv("RTLSDR_SAMPLE_RATE", 2e6) # Sample rate in Hz
 center_freq = os.getenv("RTLSDR_CENTER_FREQ", 100e6) # Center frequency in Hz
 gain = os.getenv("RTLSDR_GAIN", "auto")
 
-sdr.sample_rate = int(sample_rate)
-sdr.center_freq = int(center_freq)
-sdr.gain = "auto" if gain == "auto" else int(gain)
+sdr.setSampleRate(SOAPY_SDR_RX, 0, sample_rate)
+sdr.setFrequency(SOAPY_SDR_RX, 0, center_freq)
 
-sdr.open()
+stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
+sdr.activateStream(stream)
 
-while True:
-    samples = sdr.read_samples(sample_rate) # One second
-    print(samples)
-    break
+buf = np.array([0]*1024, np.complex64)
+
+# Stuff
+
+sdr.deactivateStream(stream)
+sdr.closeStream(stream)
